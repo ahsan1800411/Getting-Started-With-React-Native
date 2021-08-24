e35ccef3f890932b922c17e64b324716fcd14919
ahsan1800411 <ahsanmumtaz008976@gmail.com>
2021-08-24 14:54:41 +0500

    Building the ViewImage Screen

--- App.js
+++ App.js
@@ -1,7 +1,11 @@
 // import { StatusBar } from "expo-status-bar";
 import React from "react";
+import ViewImageScreen from "./app/Screens/ViewImageScreen";
 import WelcomeScreen from "./app/Screens/WelcomeScreen";
 
 export default function App() {
-  return <WelcomeScreen />;
+  return (
+    // <WelcomeScreen />
+    <ViewImageScreen />
+  );
 }
