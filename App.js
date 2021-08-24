e8104ce94760ba719d06d0d2347a5955644341f7
ahsan1800411 <ahsanmumtaz008976@gmail.com>
2021-08-24 20:19:47 +0500

    Implementing the card Component

--- App.js
+++ App.js
@@ -4,13 +4,26 @@ import { View } from "react-native";
 import ViewImageScreen from "./app/Screens/ViewImageScreen";
 import WelcomeScreen from "./app/Screens/WelcomeScreen";
 import AppButton from "./components/AppButton";
+import Card from "./components/Card";
+
+// export default function App() {
+//   return <WelcomeScreen />;
+//   // <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+//   {
+//     /* <AppButton title='Login' onPress={() => console.log("Tapped")} /> */
+//   }
+//   // </View>
+//   // <ViewImageScreen />
+// }
 
 export default function App() {
-  return <WelcomeScreen />;
-  // <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-  {
-    /* <AppButton title='Login' onPress={() => console.log("Tapped")} /> */
-  }
-  // </View>
-  // <ViewImageScreen />
+  return (
+    <View style={{ backgroundColor: "#f8f4f4", padding: 20, paddingTop: 100 }}>
+      <Card
+        title='Red Jacket For Sale'
+        subTitle='$100'
+        image={require("./app/assets/jacket.jpg")}
+      />
+    </View>
+  );
 }
