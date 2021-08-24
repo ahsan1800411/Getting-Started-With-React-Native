da6680152fe8d5d1f08a2d0f701eabb1cd215c5f
ahsan1800411 <ahsanmumtaz008976@gmail.com>
2021-08-24 18:14:48 +0500

    Building the Button Component

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 // import { StatusBar } from "expo-status-bar";
 import React from "react";
+import { View } from "react-native";
 import ViewImageScreen from "./app/Screens/ViewImageScreen";
 import WelcomeScreen from "./app/Screens/WelcomeScreen";
+import AppButton from "./components/AppButton";
 
 export default function App() {
   return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <AppButton title='Login' onPress={() => console.log("Tapped")} />
+    </View>
     // <WelcomeScreen />
-    <ViewImageScreen />
+    // <ViewImageScreen />
   );
 }
---
+++ components/AppButton.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
+import colors from "../app/config/colors";
+
+export default function AppButton({ title, onPress }) {
+  return (
+    <TouchableOpacity style={styles.button} onPress={onPress}>
+      <Text style={styles.text}>{title}</Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: colors.primary,
+    borderRadius: 25,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 15,
+    width: "100%",
+  },
+  text: {
+    color: colors.white,
+    fontWeight: "bold",
+    textTransform: "uppercase",
+    fontSize: 18,
+  },
+});
